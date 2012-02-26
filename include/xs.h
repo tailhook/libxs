afd40d189534d9a3ad5a4405afd4ab4c2789581f
Martin Sustrik <sustrik@250bpm.com>
2012-02-26 11:02:43 +0100

    Initial infrastructure for pluggableg filters
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- include/xs.h
+++ include/xs.h
@@ -151,6 +151,7 @@ XS_EXPORT int xs_getmsgopt (xs_msg_t *msg, int option, void *optval,
 
 XS_EXPORT void *xs_init ();
 XS_EXPORT int xs_term (void *context);
+XS_EXPORT int xs_plug (void *context, void *extension);
 XS_EXPORT int xs_setctxopt (void *context, int option, const void *optval,
     size_t optvallen); 
 
